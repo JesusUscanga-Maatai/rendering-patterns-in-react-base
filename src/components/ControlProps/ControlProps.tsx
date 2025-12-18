53ed7927cf01b43f360cecfc12bdc5295fd84d66
JesusUscanga-Maatai <jesus@maatai.com>
2025-12-18 17:58:34 -0600

    ADD: Lesson 1-4

--- src/components/ControlProps/ControlProps.tsx
+++ src/components/ControlProps/ControlProps.tsx
@@ -1,6 +1,31 @@
+import { useState } from "react";
+
+type ToggleProps = {
+  isToggled?: boolean;
+  onToggle?: (value: boolean) => void;
+};
+
+export const Toggle: React.FC<ToggleProps> = ({
+  isToggled = false,
+  onToggle,
+}) => {
+  const [internalToggle, setInternalToggle] = useState(isToggled);
+  const toogle = () => {
+    const newToggle = !internalToggle;
+    setInternalToggle(newToggle);
+    if (onToggle) {
+      onToggle(newToggle);
+    }
+  };
+  return <button onClick={toogle}>{internalToggle ? "ON" : "OFF"}</button>;
+};
+
 export const ParentComponent = () => {
+  const [toggleState, setToggleState] = useState(false);
   return (
     <div>
+      <p>Toggle is {toggleState ? "ON" : "OFF"}</p>
+      <Toggle isToggled={toggleState} onToggle={setToggleState} />
     </div>
   );
 };
