efd70057faa674191467c1fda3a7abe2c028fb56
JesusUscanga-Maatai <jesus@maatai.com>
2025-12-19 12:08:01 -0600

    ADD: Lesson 4

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,12 @@
+import Tab from "./Tab";
+import Tabs from "./Tabs";
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
+    <Tabs>
+      <Tab label="Jirafa">🦒</Tab>
+      <Tab label="Delfín">🐬</Tab>
+      <Tab label="Lagarto">🐊</Tab>
+    </Tabs>
   );
 };
